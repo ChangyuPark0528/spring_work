415722d5e59089918dc303d9dd44972ee0849504
Changyu <changyu1061@naver.com>
2023-10-17 17:57:12 +0900

    1017 MyWeb Join/Login/Interceptor 시작

---
+++ MyWeb/src/main/java/com/spring/myweb/user/dto/userJoinRequestDTO.java
@@ -0,0 +1,33 @@
+package com.spring.myweb.user.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Setter @Getter @ToString 
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class userJoinRequestDTO {
+	
+	private String userId;
+	private String userPw;
+	private String userName;
+	private String userPhone1;
+	private String userPhone2;
+	private String userEmail1;
+	private String userEmail2;
+	private String addrBasic;
+	private String addrDetail;
+	private String addrZipNum;
+	
+	
+}
